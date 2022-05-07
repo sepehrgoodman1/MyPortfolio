db538934c2dde6c11b6ab63126a586b7164c6c34
sepehrgoodman1 <82294076+sepehrgoodman1@users.noreply.github.com>
2022-05-07 22:31:40 +0430

    v2.1

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -14,7 +14,7 @@ function AboutMe() {
             <p className="myInfo pt-4">
               سلام من سپهر خادمی هستم، حدودا 7 ماه
               در شرکت خارجی گراب و 8 ماه در شرکت رایین به عنوان برنامه نویس <span className="bold">html css, Bootstrap, java script , React js, Next Js</span> مشغول به کار بودم بزرگترین پروژه ای که انجام دادم پروژه 
-                 یارت گالری بود که فاز اول پروژه نزدیک به 4 ماه طول کشید و با موفقیت تمام قسمت های پروژه ران شد  <a href="https://raeen-art-gallery78.vercel.app/" className="link" target="_blank">لینک پروژه</a> و اگر به کدنویسی های این پروژه دقت کنید متوجه خواهید شد که به تمیز ترین شکل ممکن کد نویسی انجام شده است <a href="https://github.com/sepehrgoodman1/Gallery-New-Project" className="link" target="_blank">لینک سورس کد</a>  
+                 ارت گالری بود که فاز اول پروژه نزدیک به 4 ماه طول کشید و با موفقیت تمام قسمت های پروژه ران شد  <a href="https://raeen-art-gallery78.vercel.app/" className="link" target="_blank">(لینک پروژه)</a> و اگر به کدنویسی های این پروژه دقت کنید متوجه خواهید شد که به تمیز ترین شکل ممکن کد نویسی انجام شده است <a href="https://github.com/sepehrgoodman1/Gallery-New-Project" className="link" target="_blank">(لینک سورس کد)</a>  
             </p>
           </div>
         </div>
